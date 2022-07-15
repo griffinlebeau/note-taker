bdc0d904cfe179a6373fcefe63ad802d6bf6b544
Griffin <griffinlebeau2@gmail.com>
2022-07-14 22:19:22 -0500

    route fixes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,18 +1,30 @@
+const fs = require('fs');
 const router = require('express').Router();
 const { validateNote, createNewNote } = require('../../lib/notes')
 const { notes } = require('../../db/db.json');
 
 
-router.get('/api/notes', (req, res) => { 
-    res.json(notes)});
+router.get('/api/notes', (req, res) => {
+    fs.readFile("db/db.json", "utf-8", (err, notes) => {
+        if (err) throw err;
+        res.json(JSON.parse(notes))
+            }
+        )
+    }
+);
 
 router.post('/api/notes', (req, res) => { 
     console.log(req.body); 
     req.body.id = notes.length.toString();
     if (validateNote(req.body)){
         res.status(400).send('The note is not properly formatted.')
-    } else {const note = createNewNote(req.body, notes);
-    res.json(note)}});
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+        }
+    }
+);
 
 
-module.exports = router;
+
+module.exports = router
