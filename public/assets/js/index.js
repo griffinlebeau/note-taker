c22282fa23125ff033fd2e595725f22305b2caf2
Griffin <griffinlebeau2@gmail.com>
2022-08-04 11:35:17 -0500

    test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,14 +2,13 @@ let noteTitle;
 let noteText;
 let saveNoteBtn;
 let newNoteBtn;
-let noteList;
+let noteList = document.querySelectorAll('.list-container .list-group');
 
 if (window.location.pathname === '/notes.html') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
 }
 
 // Show an element
